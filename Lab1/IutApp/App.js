c16612dd44c9e3185f35903d5070b47bc41c0ea2
Fahim Arsad Nafis <fahimarsad@iut-dhaka.edu>
2020-10-14 23:44:14 +0600

    Faculty List Screen is added

--- Lab1/IutApp/App.js
+++ Lab1/IutApp/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack"
 import HomeScreen from './src/screens/HomeScreen';
+import Faculty from './src/screens/Faculty';
 
 const stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <stack.Navigator>
+      <stack.Navigator initialRouteName="Home">
         <stack.Screen name="Home" component={HomeScreen} />
+        <stack.Screen name="Faculty List" component={Faculty} />
       </stack.Navigator>
     </NavigationContainer>
   )
---
+++ Lab1/IutApp/src/screens/Faculty.js
@@ -0,0 +1,58 @@
+import React from "react"
+import { Text, View, StyleSheet } from "react-native"
+import { FlatList, ScrollView } from "react-native-gesture-handler";
+
+const Faculty = () => {
+    const faculty = [
+        { name: "Professor Dr. Muhammad Mahbub Alam", key: "1" },
+        { name: "Professor Dr. Abu Raihan Mostofa Kamal", key: "2" },
+        { name: "Professor Dr. Md. Hasanul Kabir", key: "3" },
+        { name: "Professor Dr. Kamrul Hasan", key: "4" },
+        { name: "Mr. Mohayeminul Islam", key: "5" },
+        { name: "Ms. Tajkia Rahman Toma", key: "6" },
+        { name: "Mr. Ridwan Kabir", key: "7" },
+        { name: "Mr. Tasnim Ahmed", key: "8" },
+        { name: "Mr. Fardin Saad", key: "9" },
+        { name: "Md. Jubair Ibna Mostafa", key: "10" },
+        { name: "Ms. Sadia Sharmin", key: "11" },
+    ];
+    return (
+        <ScrollView style={styles.ScrollViewStyle}>
+
+            <FlatList
+                style={styles.FlatListStyle}
+                showsVerticalScrollIndicator={false}
+                data={faculty}
+                renderItem={
+                    function ({ item }) {
+                        return (
+                            <Text style={styles.TextStyle}>{item.key}.  {item.name} </Text>
+                        )
+                    }
+                }
+            />
+
+        </ScrollView>
+    );
+}
+const styles = StyleSheet.create(
+    {
+        ScrollViewStyle: {
+            backgroundColor: '#c6d7eb',
+        },
+
+        TextStyle: {
+            fontSize: 15,
+            color: '#1e3d59',
+            textAlign: "left",
+            padding: 20,
+        },
+        FlatListStyle: {
+            textAlign: 'left',
+            margin: 20,
+            backgroundColor: '#c6d7eb',
+        }
+    }
+)
+
+export default Faculty;
